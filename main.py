2d23e1fa4c0409ae7c773ae771205d4dd7d85d9e
Jessie Wang <Jessie@jessies-macbook.baruch.local>
2016-09-12 20:50:09 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from __future__ import division, print_function, absolute_import
+
+def fact(x):
+    ###
+
+def main():
+    print(fact(5))
+
+"""
+main()
+
+this is not practical.
+Use the following one:
+"""
+if __name__ == "__main__":
+    main()
